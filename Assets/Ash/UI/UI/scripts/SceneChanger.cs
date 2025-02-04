7dc81dc6df620e2b9ba95f56671ae739b3bf3d4f
Ash28s <125830669+Ash28s@users.noreply.github.com>
2025-02-05 03:07:44 +0530

    Added UI

---
+++ Assets/Ash/UI/UI/scripts/SceneChanger.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class SceneChanger : MonoBehaviour
+{
+    // Name of the scene you want to load
+    public string sceneName;
+
+    // This function will be triggered when the button is pressed
+    public void ChangeScene()
+    {
+        // Check if the scene name is not empty or null
+        if (!string.IsNullOrEmpty(sceneName))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+            Debug.LogWarning("Scene name is not set in the inspector!");
+        }
+    }
+}
