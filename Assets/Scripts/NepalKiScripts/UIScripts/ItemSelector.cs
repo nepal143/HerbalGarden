1eea3f0a616abe01c2f511c231f129d10ac65491
IgniteNS <71310255+nepal143@users.noreply.github.com>
2025-02-26 23:09:22 +0530

    sena prefab adjust ker dena thoda sorry

--- Assets/Scripts/NepalKiScripts/UIScripts/ItemSelector.cs
+++ Assets/Scripts/NepalKiScripts/UIScripts/ItemSelector.cs
@@ -1,40 +1,26 @@
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.EventSystems; // Required for pointer events
+using UnityEngine.EventSystems;
 
-public class ItemSelector : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+public class ItemSelector : MonoBehaviour, IPointerDownHandler
 {
-    public string itemName; // Name of the item (should match the inventory item)
-    public VRInventoryManager inventoryManager; // Reference to the inventory manager
+    public int itemIndex;
+    public VRInventoryManager inventoryManager;
 
     void Start()
     {
-        if (inventoryManager == null) 
+        if (inventoryManager == null)
         {
-            // If inventoryManager is not assigned in the Inspector, get it using the "Inventory" tag
-            inventoryManager = GameObject.FindGameObjectWithTag("Inventory").GetComponent<VRInventoryManager>();
+            inventoryManager = GameObject.FindGameObjectWithTag("Inventory")?.GetComponent<VRInventoryManager>();
         }
     }
 
-    // Called when the button is pressed
     public void OnPointerDown(PointerEventData eventData)
     {
         if (inventoryManager != null)
         {
-            inventoryManager.SelectItem(itemName);
-        }
-        else
-        {
-            Debug.LogError("InventoryManager not found!");
-        }
-    }
-
-    // Called when the button is released
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        if (inventoryManager != null)
-        {
-            inventoryManager.DeselectItem();
+            Debug.Log($"Item selected (Index: {itemIndex})");
+            inventoryManager.SelectItemByIndex(itemIndex);
         }
     }
 }
